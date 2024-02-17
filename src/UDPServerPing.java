951d87267f823c42028f1cfd88f43b0ea5b23b9b
Salem <73657111+salem-JUC@users.noreply.github.com>
2024-02-17 13:29:54 +0300

    Edit PrintData from UDPsServer.java

--- src/UDPServerPing.java
+++ src/UDPServerPing.java
@@ -33,26 +33,11 @@ public class UDPServerPing {
 
     private static void printData(DatagramPacket request) throws Exception
     {
-        // Obtain references to the packet's array of bytes.
         byte[] buf = request.getData();
-
-        // Wrap the bytes in a byte array input stream,
-        // so that you can read the data as a stream of bytes.
         ByteArrayInputStream bais = new ByteArrayInputStream(buf);
-
-        // Wrap the byte array output stream in an input stream reader,
-        // so you can read the data as a stream of characters.
         InputStreamReader isr = new InputStreamReader(bais);
-
-        // Wrap the input stream reader in a bufferred reader,
-        // so you can read the character data a line at a time.
-        // (A line is a sequence of chars terminated by any combination of \r and \n.)
         BufferedReader br = new BufferedReader(isr);
-
-        // The message data is contained in a single line, so read this line.
         String line = br.readLine();
-
-        // Print host address and data received from it.
         System.out.println(
                 "Received from " +
                         request.getAddress().getHostAddress()
